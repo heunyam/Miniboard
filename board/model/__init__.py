85972977035461f2667b95888984bbe7e026e1c0
heunyam <tpwns072@gmail.com>
2020-11-05 23:00:05 +0900

    [feat] pool recycle 3600 으로 수정

--- board/model/__init__.py
+++ board/model/__init__.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 
 from board.config import DATABASE_URL
 engine = create_engine(DATABASE_URL, echo=True, encoding="utf-8", pool_size=20,
-                       pool_recycle=500, max_overflow=20, pool_pre_ping=True)
+                       pool_recycle=3600, max_overflow=20, pool_pre_ping=True)
 
 Session = scoped_session(sessionmaker(bind=engine, autoflush=False, autocommit=False))
 session = Session()
