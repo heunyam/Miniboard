3b5c28d64caf356a43e043a152e2426f3287f407
heunyam <tpwns072@gmail.com>
2020-11-05 22:57:36 +0900

    [feat] sqlalchemy 세션 기본 셋팅

---
+++ board/model/__init__.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+from board.config import DATABASE_URL
+engine = create_engine(DATABASE_URL, echo=True, encoding="utf-8", pool_size=20,
+                       pool_recycle=500, max_overflow=20, pool_pre_ping=True)
+
+Session = scoped_session(sessionmaker(bind=engine, autoflush=False, autocommit=False))
+session = Session()
+
+Base = declarative_base()
+Base.query = session.query_property()
