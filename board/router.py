e8900fdcb448d59e40b42b65d0f2c42e50629774
heunyam <tpwns072@gmail.com>
2020-11-06 19:24:42 +0900

    [feat] get post list api routing

--- board/router.py
+++ board/router.py
@@ -2,8 +2,10 @@ from flask import Blueprint
 from flask_restful import Api
 
 from board.view.ping import Ping
+from board.view.post import Post
 
 bp = Blueprint("ping", __name__, url_prefix="")
 api = Api(bp)
 
 api.add_resource(Ping, "/ping")
+api.add_resource(Post, '/post')
