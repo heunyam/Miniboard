51911002653fe0e0129af54af8380096e1dc60e8
heunyam <tpwns072@gmail.com>
2020-11-20 17:22:52 +0900

    [fix] mongo 제거 mysql 로 변경

--- board/controller/version.py
+++ board/controller/version.py
@@ -1,8 +1,10 @@
-from board.model import mongodb
+from board.model import session
+from board.model.android_info import AndroidInfo
 
 
 def check_recently_version(android_version):
-    current_version = mongodb.android_info.find()[0]['version']
+
+    current_version = session.query(AndroidInfo).order_by(AndroidInfo.created_at.desc()).first()
 
     return {
         'is_lastest': True if android_version == current_version else False
